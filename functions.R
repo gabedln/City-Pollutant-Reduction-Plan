68f450a184a83cac37f6163c06af1ceee0b9e578
gabedln <mddeleon7@up.edu.ph>
2025-11-18 02:26:06 +0800

    adjusted Simplex matrix and tracked tableau and basicSolution history

--- functions.R
+++ functions.R
@@ -21,7 +21,7 @@ FindMin = function(c){ # finds the minimum index in a vector (in this case, the 
 
 FindPivotRow = function(pcol_index, tableau){ # finds the pivot row in the tableau by calculating TR
   MinTR = Inf # initialization for comparison purposes only
-  prow_index = 1 # similar to FindMin
+  prow_index = -1 # similar to FindMin
   for(i in 1:(nrow(tableau)-1)){ # iterates through every row in the tableau
     if(tableau[i,pcol_index]<=0){ # Only looks at the elements in the pivot column
       next # If value is less than or equal to 0 moves to next iteration
@@ -56,10 +56,15 @@ BasicSolutionChecker = function(current_column){ # Checks for basic solution in 
 }
 
 Simplex = function(tableau, isMax){ # tableau is matrix of initial, isMax deterimines whether max or min
-  basic_solution = c()
+  basic_solution_history = list()
+  tableau_history = list()
   while(NegativeCheck(tableau)){ # While there is still negative in the last row
+    basic_solution = c() # For basic solution of that iteration
     pcol_index = FindMin(tableau[nrow(tableau),]) # Uses FindMin function
     prow_index = FindPivotRow(pcol_index, tableau) # Uses FindPivotRow function
+    if(prow_index==-1){
+      return (NA)
+    }
     tableau[prow_index,] = tableau[prow_index,] / tableau[prow_index,pcol_index] # normalizes pivot row
     for(i in 1:nrow(tableau)){ #elimination
       if(i==prow_index){
@@ -67,31 +72,32 @@ Simplex = function(tableau, isMax){ # tableau is matrix of initial, isMax deteri
       }
       tableau[i,] = tableau[i,] - (tableau[prow_index,]*tableau[i,pcol_index])
     }
-    print(tableau)
-  }
-  if(isMax){ # Way of determining basic solution for maximization
-    for(i in 1:(ncol(tableau)-1)){
-      index = BasicSolutionChecker(tableau[,i])
-      if(index!=-1){ #Iterates through every column, if that column returns True
-        basic_solution = c(basic_solution, tableau[index, ncol(tableau)])
-      } else {
-        basic_solution = c(basic_solution, 0) # If false (meaning more than 1 nonzero), add 0 to basic solution vector
+    tableau_history[[length(tableau_history)+1]] = tableau
+    
+    if(isMax){ # Way of determining basic solution for maximization
+      for(i in 1:(ncol(tableau)-1)){
+        index = BasicSolutionChecker(tableau[,i])
+        if(index!=-1){ #Iterates through every column, if that column returns True
+          basic_solution = c(basic_solution, tableau[index, ncol(tableau)])
+        } else {
+          basic_solution = c(basic_solution, 0) # If false (meaning more than 1 nonzero), add 0 to basic solution vector
+        }
       }
-    }
-    labeled_list = list(finalTableau = tableau, basicSolution = matrix(basic_solution, nrow=1, byrow=TRUE), Z = basic_solution[length(basic_solution)]) # initializes labeled list
-  } else { # If minimization
-    for(i in 1:ncol(tableau)){ # iterates through columns
-      if(i==(ncol(tableau)-1)){ # if at Z column, skips iteration
-        next
+      basic_solution_history[[length(basic_solution_history)+1]] = basic_solution
+    } 
+    
+    else { # If minimization
+      for(i in 1:ncol(tableau)){ # iterates through columns
+        if(i==(ncol(tableau)-1)){ # if at Z column, skips iteration
+          next
+        }
+        basic_solution = c(basic_solution, tableau[nrow(tableau), i]) # Adds the element of last row, and column i to basic solution
       }
-      basic_solution = c(basic_solution, tableau[nrow(tableau), i]) # Adds the element of last row, and column i to basic solution
+      basic_solution_history[[(length(basic_solution_history)+1)]] = basic_solution
     }
-    labeled_list = list(finalTableau = tableau, basicSolution = matrix(basic_solution, nrow=1, byrow=TRUE), Z = tableau[nrow(tableau),ncol(tableau)]) # initializes labeled list
-  }
+    
+  } # end of while loop bracket
+  basicSolution = basic_solution_history[[length(basic_solution_history)]]
+  labeled_list = list(finalTableau = tableau, basicSolution = basicSolution, Z = basicSolution[length(basicSolution)], tableau_history = tableau_history, basic_solution_history = basic_solution_history) # initializes labeled list
   return(labeled_list)
 }
-
-maximizationExample = matrix(c(20,25,1,0,0,0,0,8000,650,300,0,1,0,0,0,200000,1,0,0,0,1,0,0,300,0,1,0,0,0,1,0,250,-10500,-12000,0,0,0,0,1,0),nrow=5, ncol=8, byrow=TRUE)
-
-
-Simplex(maximizationExample, TRUE)
